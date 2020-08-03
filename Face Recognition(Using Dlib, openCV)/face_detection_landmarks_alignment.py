c71726cb16faf5ae67a1e667026189afade2e4e9
anitabaral <baralanita2@gmail.com>
2020-08-03 16:15:40 +0545

    solved the merged problems by resetting and then added code detail info

---
+++ Face Recognition(Using Dlib, openCV)/face_detection_landmarks_alignment.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import sys
+import dlib
+import cv2
+import openface
+from skimage import io
+
+
+# In[39]:
+
+def align_image(img):
+
+    predictor_model = "shape_predictor_68_face_landmarks.dat"
+    face_aligner = openface.AlignDlib(predictor_model)
+    alignedFace = face_aligner.align(
+        96,
+        img,
+        face_aligner.getLargestFaceBoundingBox(img),
+        landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
+
+    return alignedFace
+
+def align_image_new(img, face_rect):
+
+    predictor_model = "shape_predictor_68_face_landmarks.dat"
+    face_aligner = openface.AlignDlib(predictor_model)
+    alignedFace = face_aligner.align(
+        96,
+        img,
+        face_rect,
+        landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
+
+    return alignedFace
+
